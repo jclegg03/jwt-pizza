2fdb93992dd8c3e09987bae66e8943f1499cd3f6
Jay <sheevpa1patine1024@gmail.com>
2026-02-02 19:36:17 -0700

    write basic pizza order test

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -5,3 +5,23 @@ test('home page', async ({ page }) => {
 
   expect(await page.title()).toBe('JWT Pizza');
 });
+
+test('purchase with login', async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole('link', { name: 'Order' }).click();
+  await page.getByRole('combobox').selectOption('28');
+  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('j');
+  await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('c');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+  await page.getByRole('textbox', { name: 'Password' }).press('Enter');
+  await page.getByRole('button', { name: 'Pay now' }).click();
+  await page.getByRole('button', { name: 'Verify' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+});
