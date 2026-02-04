6251baca20b7a70c7e9220d51f346f2020919dac
Jay <sheevpa1patine1024@gmail.com>
2026-02-03 20:43:50 -0700

    start writing expectations for pizza ordering 61%

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -5,12 +5,21 @@ import { Page } from '@playwright/test';
 test('order pizza', async ({ page }) => {
     await setup(page);
 
-    await page.getByRole('link', { name: 'Order' }).click();
+    expect(page.getByRole('button', { name: 'Checkout' })).toBeDisabled();
 
-    
+    await page.getByRole('combobox').selectOption('1');
+    await page.getByRole('link', { name: 'Order' }).click();
+    await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+    await page.getByRole('button', { name: 'Checkout' }).click();
+
+    await expect(page.locator('tbody')).toContainText('Veggie');
+
+    await expect(page.locator('tfoot')).toContainText('1 pie');
+    await page.getByRole('button', { name: 'Pay now' }).click();
 });
 
 async function setup(page: Page) {
     await login(page);
-    await mockAPI(page, 'menu', 'franchises');
+    await mockAPI(page, 'menu', 'franchises', 'me');
+    await page.getByRole('link', { name: 'Order' }).click();
 }
