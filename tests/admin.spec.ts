15f48772fe319fe949af5118b5850a540f0f4c19
Jay <sheevpa1patine1024@gmail.com>
2026-02-05 17:45:11 -0700

    remove unused variable

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from 'playwright-test-coverage';
+import { test } from 'playwright-test-coverage';
 import { mockAPI, login } from './utils';
 import { BrowserContext, Page } from '@playwright/test';
 
